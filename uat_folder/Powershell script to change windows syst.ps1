c14d1908db20b7f0a5374012689bf00467e8aac4
Manoj Pedekar <mpedekar@sscinc.com>
2026-05-16 13:18:52 +0530

    deleted uat folder test folder

--- uat_folder/Powershell script to change windows syst.ps1
+++
@@ -1,8 +0,0 @@
-Powershell script to change windows system language to english (United States)
-```powershell                                                                                                                                                                                                       
-# Check if the current user has administrative privileges
-if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::Get       Current()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-{
-    Write-Warning "You do not have administrator privileges. Please run this script as an administrator."
-    exit
-}   
